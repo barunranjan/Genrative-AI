bc2c2e261902477ae8f8748ad83b87a3bb0b6268
Barun Ranjan <barunranjan@Baruns-MacBook-Air.local>
2026-06-17 21:57:02 +0530

    added system prompt

--- hello_world/openAIGemini.py
+++ hello_world/openAIGemini.py
@@ -13,7 +13,8 @@ client = OpenAI(
 response = client.chat.completions.create(
     model="gemini-2.5-flash",
     messages=[
-        {"role":"user", "content":"Hey there, can you tell me the llm model name, through which you are sending response"}
+        {"role":"system", "content":"You are Math expert and you should only only answer math related question nothing else"},
+        {"role":"user", "content":"give me answer of a + b whole square"}
     ]
 )
 print(response.choices[0].message.content)

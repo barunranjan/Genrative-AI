73edefc801ba1a18acabfa8e5f97335be7f95584
Barun Ranjan <vrnranjan22@gmail.com>
2026-06-29 22:36:33 +0530

    making local api to ollam

--- ollam_fastAPI/server.py
+++ ollam_fastAPI/server.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Body
+from ollama import Client
+
 
 app = FastAPI()
+client = Client(
+    host="http://localhost:11434"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+)
+
+@app.post("/chat")
+def chat(
+    message: str = Body(..., description="The Message")
+):
+    response=client.chat(model="gemma2:2b", messages=[
+        {"role":"user", "content":message}
+    ])
+
+    return {"response":response.message.content}
